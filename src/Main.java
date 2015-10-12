3d18b445dddcf20c4441312fb348fa7408c28263
Hongda Zeng <hongda.code@gmail.com>
2015-10-12 01:52:45 -0400

    Working program, number generator now works

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.*;
 import javafx.scene.*;
@@ -12,7 +11,7 @@ public class Main extends Application {
 
     Random random = new Random();
     Stage window;
-    Scene scenemain, scenenumber, scenequote;
+    Scene sceneMain, sceneNumber, sceneQuote;
 
     public static void main(String[] args) {
         launch(args);
@@ -24,26 +23,47 @@ public class Main extends Application {
         window = primaryStage;
 
         Label labelMain = new Label("Welcome to the main menu, choose an option");
+        Label labelNumber1 = new Label("This is a random number generator (1 to 1000)");
+        Label labelQuote1 = new Label("This will display a quote");
+
 
         //Navigational buttons
         Button buttonNumber = new Button("Go to the number generator");
         Button buttonQuote = new Button("Read a random quote");
         Button returnToMain = new Button("Return to Main");
+        Button returnToMain2 = new Button("Return to Main");
+
+        buttonNumber.setOnAction(e -> window.setScene(sceneNumber));
+        buttonQuote.setOnAction(e -> window.setScene(sceneQuote));
+        returnToMain.setOnAction(e -> window.setScene(sceneMain));
+        returnToMain2.setOnAction(e -> window.setScene(sceneMain));
 
         //Action buttons
-        Button genNumber = new Button("Generate a number between 1 and 1000");
+        Button genNumber = new Button("Click to generate a number");
+        genNumber.setOnAction(e->{
+            int a = getNumber(1,1000);
+            labelNumber1.setText("You number is :" + a);
+        });
         Button getQuote = new Button("Get a new quote");
 
         VBox layoutMain = new VBox(20);
         layoutMain.getChildren().addAll(labelMain, buttonNumber, buttonQuote);
-        scenemain = new Scene(layoutMain, 450, 300);
+        sceneMain = new Scene(layoutMain, 450, 300);
 
-        window.setScene(scenemain);
+        VBox layoutNumber = new VBox(20);
+        layoutNumber.getChildren().addAll(labelNumber1,genNumber,returnToMain);
+        sceneNumber = new Scene(layoutNumber,450,300);
+
+        VBox layoutQuote = new VBox(20);
+        layoutQuote.getChildren().addAll(labelQuote1,getQuote,returnToMain2);
+        sceneQuote = new Scene(layoutQuote,450,300);
+
+        window.setScene(sceneMain);
         window.setTitle("Fun GUI");
         window.show();
     }
 
-    public int getNumber(int a, int b) {
+    public int getNumber(int b, int a) {
         int someInt;
         someInt = random.nextInt(a - b + 1) + 1;
         return someInt;
