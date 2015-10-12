6d2b8c7a6e46ee80c189d1cec2168c07bad1ac66
Hongda Zeng <hongda.code@gmail.com>
2015-10-12 01:20:07 -0400

    Working program, main only

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@ import javafx.application.Application;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.*;
 import javafx.scene.*;
 
 import java.util.Random;
 
-public class FunGui extends Application {
+public class Main extends Application {
 
+    Random random = new Random();
     Stage window;
-    Scene main, number, quote;
+    Scene scenemain, scenenumber, scenequote;
 
     public static void main(String[] args) {
         launch(args);
@@ -32,11 +34,16 @@ public class FunGui extends Application {
         Button genNumber = new Button("Generate a number between 1 and 1000");
         Button getQuote = new Button("Get a new quote");
 
-        StackPane layout = new StackPane();
+        VBox layoutMain = new VBox(20);
+        layoutMain.getChildren().addAll(labelMain, buttonNumber, buttonQuote);
+        scenemain = new Scene(layoutMain, 450, 300);
+
+        window.setScene(scenemain);
+        window.setTitle("Fun GUI");
+        window.show();
     }
 
     public int getNumber(int a, int b) {
-        Random random = new Random();
         int someInt;
         someInt = random.nextInt(a - b + 1) + 1;
         return someInt;
